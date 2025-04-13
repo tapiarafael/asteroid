2c2a12ac2d6ff783a7fd86b794eb6d6089fab19f
Rafael Tapia <rafaelsilvatapia@gmail.com>
2025-04-13 18:13:04 -0300

    feat: implement asteroids

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from constants import *
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
   print_start_info()
@@ -10,9 +11,12 @@ def main():
   clock = pygame.time.Clock()
   dt = 0
 
-  updatable, drawable = pygame.sprite.Group(), pygame.sprite.Group()
+  updatable, drawable, asteroids = pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()
   Player.containers = (updatable, drawable)
-
+  Asteroid.containers = (updatable, drawable, asteroids)
+  AsteroidField.containers = (updatable,)
+  
+  field = AsteroidField()
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
   while True:
