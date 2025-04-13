9a587f94ebcbe8b979955671c4ea5ca75bdbdbbe
Rafael Tapia <rafaelsilvatapia@gmail.com>
2025-04-13 17:38:45 -0300

    feat: implement player class and draw it on middle of screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -9,12 +10,15 @@ def main():
   clock = pygame.time.Clock()
   dt = 0
 
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
       
     screen.fill(color="black")
+    player.draw(screen)
     pygame.display.flip()
 
     dt = clock.tick(60) / 1000
@@ -22,7 +26,7 @@ def main():
 def print_start_info():
   print("Starting Asteroids!")
   print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height`: {SCREEN_HEIGHT}")
+  print(f"Screen height: {SCREEN_HEIGHT}")
   
 
 if __name__ == "__main__":
