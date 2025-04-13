e8b779048804778609d19d9910ed0efb553ee987
Rafael Tapia <rafaelsilvatapia@gmail.com>
2025-04-13 18:36:30 -0300

    feat: limit the shoot

--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOT_SPEED, PLAYER_SHOOT_COOLDOWN
 from shot import Shot
 
 class Player(CircleShape):
   def __init__(self, x, y):
     super().__init__(x, y, PLAYER_RADIUS)
     self.rotation = 0
+    self.shoot_timer = 0.0
 
   def draw(self, screen):
     return pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -19,10 +20,15 @@ class Player(CircleShape):
     self.position += forward * PLAYER_SPEED * dt
 
   def shoot(self):
+    if self.shoot_timer > 0:
+      return
+    
+    self.shoot_timer = PLAYER_SHOOT_COOLDOWN;
     shot = Shot(self.position.x, self.position.y)
     shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
 
   def update(self, dt):
+    self.shoot_timer -= dt
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_a]:
